442d2c3bf986ac0cb964bedc834905e3669ebb2b
J. Gabriel Lima <jgabriel.ufpa@gmail.com>
2014-06-12 23:38:45 -0300

    config mongoHQ

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    db: 'mongodb://localhost/mean-dev',
+    db: 'mongodb://teste:teste@linus.mongohq.com:10050/jgmongodb',
     app: {
         name: 'MEAN - FullStack JS - Development'
     },
